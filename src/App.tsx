107843d929c1019090445a2c1c12d994462b1d26
Shugo Saito <shugo.saito@rakuten.com>
2019-02-01 17:30:09 +0900

    updated - added rex-icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// TODO: Not working, need type definition file
+// import RexButton from 'rex-button';
+
+/* tslint:disable-next-line */
+const RexIcons = require('rex-icons').default;
+/* tslint:disable-next-line */
+const RexButton = require('rex-button').default;
+
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <RexIcons name="logo" />
         </header>
       </div>
     );
